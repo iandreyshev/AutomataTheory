148f682761bf4091fbea223d477bd56fea825607
IvanAndreyshev-NOTEBOOK <vanyha360@yandex.ru>
2017-11-24 20:59:07 +0300

    MiniJava alphabet

---
+++ Lexer/Lexer/Lexer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexer
+{
+	class Lexer
+	{
+		private static Dictionary<string, Action> _tokens = new Dictionary<string, Action>
+		{
+			{"45", d}
+		};
+
+		public static void d()
+		{
+		}
+	}
+}
